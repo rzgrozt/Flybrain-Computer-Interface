99b17d45bc7628bdc279a7d1a98cb9035d6dac59
RzgrOzt <neuruzgar@gmail.com>
2026-09-22 13:23:06 +0200

    refactor: export sandbox environment and desktop adapters

--- src/flybrain_interface/environment/__init__.py
+++ src/flybrain_interface/environment/__init__.py
@@ -1,1 +1,41 @@
-"""Computer and controlled training environments."""
+"""Local environment interfaces."""
+
+from flybrain_interface.environment.cursor import CursorState, VirtualCursorEnvironment
+from flybrain_interface.environment.kwin_cursor import CursorPosition, KWinCursorBridge
+from flybrain_interface.environment.pointer import (
+    DryRunPointerAdapter,
+    PointerCapability,
+    PointerMove,
+    UInputPointerAdapter,
+    create_pointer_adapter,
+    uinput_capability,
+)
+from flybrain_interface.environment.screen import (
+    ColorTargetDetector,
+    DetectedTarget,
+    ScreenCaptureCapability,
+    ScreenRegion,
+    SpectacleScreenCapture,
+    crop_frame,
+    spectacle_capability,
+)
+
+__all__ = [
+    "ColorTargetDetector",
+    "CursorPosition",
+    "CursorState",
+    "DetectedTarget",
+    "DryRunPointerAdapter",
+    "KWinCursorBridge",
+    "PointerCapability",
+    "PointerMove",
+    "ScreenCaptureCapability",
+    "ScreenRegion",
+    "SpectacleScreenCapture",
+    "UInputPointerAdapter",
+    "VirtualCursorEnvironment",
+    "create_pointer_adapter",
+    "crop_frame",
+    "spectacle_capability",
+    "uinput_capability",
+]
